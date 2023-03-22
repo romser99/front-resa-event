78f1a9956cf6185ca50abb401aa9c66a65788508
romser99 <rom.serrano99@gmail.com>
2023-03-22 16:46:12 +0100

    tp bouton civilite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'front-resa-event';
+  compteur = 6;
+  libelle = "Le compteur initial est";
+  civilite = ""
+
+  miseAjourDuCompteur(increment: number){
+    this.compteur = increment === 1 ? this.compteur + 1 : this.compteur - 1
+  }
+  majCivilite(valeur : number){
+    if (valeur == 1){
+      this.civilite= "Monsieur";
+    }
+    else if (valeur == -1){
+      this.civilite = "Madame"
+    }
+    else {
+      this.civilite = "Roll"
+
+    }
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibelleCompteurComponent } from './libelle-compteur/libelle-compteur.component';
+import { EditionCompteurComponent } from './edition-compteur/edition-compteur.component';
+import { CiviliteComponent } from './civilite/civilite.component';
+import { EditionCiviliteComponent } from './edition-civilite/edition-civilite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibelleCompteurComponent,
+    EditionCompteurComponent,
+    CiviliteComponent,
+    EditionCiviliteComponent
   ],
   imports: [
     BrowserModule,
